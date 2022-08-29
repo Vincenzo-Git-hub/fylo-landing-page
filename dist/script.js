2d7868f143fb504f8d297bb0b5f9c8bddb10ac4b
Vincenzo-Git-hub <76475312+Vincenzo-Git-hub@users.noreply.github.com>
2022-08-29 12:10:11 +0200

    finished project

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-console.log("hello");
+function reveal(){for(var e=document.querySelectorAll(".reveal"),t=0;t<e.length;t++){var n=window.innerHeight;e[t].getBoundingClientRect().top<n-150?e[t].classList.add("active"):e[t].classList.remove("active")}}window.addEventListener("scroll",reveal);
 //# sourceMappingURL=script.js.map
